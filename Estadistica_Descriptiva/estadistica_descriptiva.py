2bccf7a615987de0f3843654312733efbc2533a3
gregorio <gregorio.rico.gregorio@gmail.com>
2024-03-09 03:54:34 -0600

    Cambios estadistica descriptiva e imagenes

--- Estadistica_Descriptiva/estadistica_descriptiva.py
+++ Estadistica_Descriptiva/estadistica_descriptiva.py
@@ -1,24 +1,59 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from scipy.stats import skew, kurtosis
 
 data = pd.read_csv('Datos/Streaming_History_Audio.csv')
 
+data['ts'] = pd.to_datetime(data['ts'])  # Convertir la columna 'ts' a formato datetime
+data['minutes_played'] = data['ms_played'] / 60000  # Convertir 'ms_played' a minutos
+
+def visualizar_reproducciones_por_fecha(data):
+    fig, ax = plt.subplots(figsize=(15, 8))
+    data.groupby(data['ts'].dt.date)['minutes_played'].sum().plot(kind='line', ax=ax)
+    ax.set(title='Reproducciones Diarias', xlabel='Fecha', ylabel='Minutos Reproducidos')
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+
+
+    fig, ax = plt.subplots(figsize=(15, 8))
+    data_grouped = data.groupby(data['ts'].dt.date)['minutes_played'].sum().reset_index()
+    data_grouped['ts'] = pd.to_datetime(data_grouped['ts'])
+    ax.scatter(data_grouped['ts'], data_grouped['minutes_played'], alpha=0.5, edgecolors='none')
+    plt.gcf().autofmt_xdate()
+    ax.set_title('Tiempo de reproducción por fecha')
+    ax.set_xlabel('Fecha')
+    ax.set_ylabel('Minutos reproducidos')
+
+    plt.show()
+
+
 def calcular_estadisticas_descriptivas(duracion):
     estadisticas = {
-        'Minimo': duracion.min(),
-        'Maximo': duracion.max(),
-        'Moda': duracion.mode()[0],
+        'Mínimo': duracion.min(),
+        'Máximo': duracion.max(),
+        'Moda': calcular_moda(duracion),
         'Conteo': duracion.count(),
         'Sumatoria': duracion.sum(),
         'Media': duracion.mean(),
         'Varianza': duracion.var(),
-        'Desviacion Estandar': duracion.std(),
-        'Asimetria': skew(duracion, bias=False),
+        'Desviación Estándar': duracion.std(),
+        'Asimetría': skew(duracion, bias=False),
         'Curtosis': kurtosis(duracion, bias=False)
     }
     return pd.DataFrame([estadisticas], index=["Estadísticas"])
 
-# Menú Principal
+def calcular_moda(serie):
+    moda = serie.mode()
+    if len(moda) > 1:
+        moda = ", ".join(moda)
+    else:
+        moda = moda[0]
+    return moda
+
 def menu_principal():
     opcion = ''
     while opcion != '5':
@@ -26,76 +61,79 @@ def menu_principal():
         print("1. Canciones")
         print("2. Álbumes")
         print("3. Artistas")
-        print("4. Filtros")
+        print("4. Filtro")
         print("5. Salir")
         opcion = input("Selecciona una opción: ")
 
         if opcion == '1':
-            menu_canciones()
+            menu_generico('Canciones', 'master_metadata_track_name')
         elif opcion == '2':
-            menu_albumes()
+            menu_generico('Álbumes', 'master_metadata_album_album_name')
         elif opcion == '3':
-            menu_artistas()
+            menu_generico('Artistas', 'master_metadata_album_artist_name')
         elif opcion == '4':
-            menu_filtros()
+            visualizar_reproducciones_por_fecha(data)
         elif opcion == '5':
-            print("Saliendo del programa...")
+            return
         else:
             print("Opción no válida. Por favor, intenta nuevamente.")
 
-# Menú y funciones para Canciones
-def menu_canciones():
+def menu_generico(tipo, columna):
+
     opcion = ''
+
     while opcion != '3':
-        print("\nMenú Canciones")
+        print(f"\nMenú {tipo}")
         print("1. Ver estadísticas generales")
-        print("2. Buscar canción específica")
+        print("2. Buscar específico")
         print("3. Salir")
         opcion = input("Selecciona una opción: ")
 
         if opcion == '1':
-            estadisticas_canciones(data)
+            estadisticas_generales(data, columna, tipo)
         elif opcion == '2':
-            buscar_cancion(data)
-        elif opcion == '3':
-            break
+            buscar_especifico(data, columna, tipo)
         else:
             print("Opción no válida. Por favor, intenta nuevamente.")
 
-def estadisticas_canciones(data):
-    print("\nEstadísticas Descriptivas de Canciones:")
-    duracion_segundos = data['ms_played'] / 1000
+def estadisticas_generales(data, columna, tipo):
+    print(f"\nEstadísticas Descriptivas por {columna}:")
+    duracion_segundos = data.groupby(columna)['ms_played'].sum() / 60000
     estadisticas_df = calcular_estadisticas_descriptivas(duracion_segundos)
-    print(estadisticas_df)
+    print(estadisticas_df.transpose())
 
-def buscar_cancion(data):
-    nombre_cancion = input("Ingresa el nombre de la canción que deseas buscar: ")
-    canciones_filtradas = data[data['master_metadata_track_name'].str.contains(nombre_cancion, case=False)]
-    if canciones_filtradas.empty:
+    visualizar_reproducciones_por_fecha(data)
+
+def calcular_moda_canciones(data):
+    moda = data.value_counts().idxmax()  
+    return moda
+
+def buscar_especifico(data, columna, tipo):
+    nombre = input(f"Ingresa el nombre de {columna} que deseas buscar: ")
+    filtrados = data[data[columna].str.contains(nombre, case=False)]
+    
+    if filtrados.empty:
         print("No se encontraron coincidencias.")
-    else:
-        print(f"\nEstadísticas para '{nombre_cancion}':")
-        duracion_segundos = canciones_filtradas['ms_played'] / 1000
-        estadisticas_df = calcular_estadisticas_descriptivas(duracion_segundos)
-        print(estadisticas_df)
+        return
 
-# Implementaciones para menú de álbumes y artistas serían similares a las de canciones
+    if tipo == 'Canciones':
+        minimo = filtrados['ms_played'].min()
+        maximo = filtrados['ms_played'].max()
+        moda = calcular_moda_canciones(filtrados['ms_played'])
 
-# Menú y funciones para Álbumes
-def menu_albumes():
-    # Esta función seguiría un patrón similar al menu_canciones, adaptado para álbumes
-    pass
+        print(f"\nEstadísticas para '{nombre}':")
+        print(f"Mínimo (ms_played): {minimo}")
+        print(f"Máximo (ms_played): {maximo}")
+        print(f"Moda (ms_played): {moda}")
 
-# Menú y funciones para Artistas
-def menu_artistas():
-    # Esta función seguiría un patrón similar al menu_canciones, adaptado para artistas
-    pass
+    elif tipo == 'Álbumes' or tipo == 'Artistas':
+        cancion_minimo = filtrados.loc[filtrados['ms_played'].idxmin(), 'master_metadata_track_name']
+        cancion_maximo = filtrados.loc[filtrados['ms_played'].idxmax(), 'master_metadata_track_name']
+        cancion_moda = calcular_moda_canciones(filtrados['master_metadata_track_name'])
 
-# Menú y funciones para Filtros
-def menu_filtros():
-    # Implementación del menú de filtros y aplicación
-    pass
+        print(f"\nEstadísticas para '{nombre}':")
+        print(f"Mínimo (Canción): {cancion_minimo}")
+        print(f"Máximo (Canción): {cancion_maximo}")
+        print(f"Moda (Canción): {cancion_moda}")
 
-# Iniciar el programa
-if __name__ == "__main__":
-    menu_principal()
+menu_principal()
